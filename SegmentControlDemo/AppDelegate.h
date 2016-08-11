03c565b036c2a90092c96b4ebe759480bcac61e8
shenjiaxiong <shenjiaxiong@whty.com.cn>
2016-08-11 16:27:44 +0800

    Initial Commit

---
+++ SegmentControlDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SegmentControlDemo
+//
+//  Created by boolean on 16/8/11.
+//  Copyright © 2016年 boolean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SegmentControlDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SegmentControlDemo
+//
+//  Created by boolean on 16/8/11.
+//  Copyright © 2016年 boolean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
